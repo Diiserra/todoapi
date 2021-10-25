828dede5773bb1a6847d013553fbe363e0d1cced
Diego Lima <diiserras@gmail.com>
2021-10-25 16:53:41 -0300

    Criação de um perfil de testes e um banco em memória usando o H2

--- src/main/java/com/todoapi/todoapi/model/User.java
+++ src/main/java/com/todoapi/todoapi/model/User.java
@@ -1,7 +1,15 @@
 package com.todoapi.todoapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity()
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
     private String name;
     private String email;
